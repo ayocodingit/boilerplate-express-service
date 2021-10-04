961fc77c39e060008091a1f403974c3d95270e8f
ayocodingit <ayocodingit@gmail.com>
2021-10-04 21:10:26 +0700

    remove return send mail service

--- src/modules/email/service/index.ts
+++ src/modules/email/service/index.ts
@@ -2,5 +2,5 @@ import { Mail } from '../entity'
 import { sendMailRepository } from '../repository'
 
 export const sendMailService = (data: Mail) => {
-  return sendMailRepository(data)
+  sendMailRepository(data)
 }
