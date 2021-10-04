9d98945ced1f03eda2b1272de0efcf24824a8648
ayocodingit <ayocodingit@gmail.com>
2021-10-04 08:04:53 +0700

    update knex production

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -18,6 +18,23 @@ export default {
       tableName: 'migrations'
     }
   },
+  production: {
+    client: config.get('db.connection'),
+    connection: {
+      host: config.get('db.host'),
+      port: config.get('db.port'),
+      user: config.get('db.user'),
+      password: config.get('db.password'),
+      database: config.get('db.database')
+    },
+    pool: {
+      min: Number(config.get('db.pool.min', 10)),
+      max: Number(config.get('db.pool.max', 100))
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  },
   testing: {
     client: config.get('db.connection'),
     connection: {
