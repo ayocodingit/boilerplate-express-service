3ddcaa58d9b4f41d517e7a31d8d6e80c58630a98
ayocodingit <ayocodingit@gmail.com>
2021-10-04 13:05:14 +0700

    add config jest

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,20 +1,30 @@
-export default {
-  clearMocks: true,
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest'
-  },
-  setupFilesAfterEnv: ['jest-extended'],
-  globals: {
-    'ts-jest': {
-      diagnostics: false
-    }
-  },
-  globalSetup: './config/global-setup.ts',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    './modules/**/*.{ts,js}',
-    '!**/node_modules/**'
-  ],
-  coverageReporters: ['lcov']
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    bail: 1,
+    verbose: true,
+    clearMocks: true,
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.ts?$': 'ts-jest'
+    },
+    setupFilesAfterEnv: ['jest-extended'],
+    globals: {
+      'ts-jest': {
+        diagnostics: false
+      }
+    },
+    globalSetup: './config/global-setup.ts',
+    collectCoverage: true,
+    collectCoverageFrom: [
+      './modules/**/*.ts',
+      '!./modules/**/handler/*.ts',
+      '!./modules/email/**/*.ts',
+      '!./modules/oauth/**/*.ts',
+      '!**/node_modules/**'
+    ],
+    coverageReporters: ['lcov'],
+    detectOpenHandles: true
+  }
 }
