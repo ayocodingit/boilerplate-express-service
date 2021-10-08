e41ea283b27f247773e92ab43bb87a74dfd127ac
ayocodingit <ayocodingit@gmail.com>
2021-10-08 07:30:11 +0700

    remove mongo

--- src/config/mongo.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from 'mongoose'
-import config from '.'
-
-mongoose.connect(config.get('mongo.connection'))
-
-export default (document: string, schema: Schema) => {
-  return mongoose.model(document, schema)
-}
